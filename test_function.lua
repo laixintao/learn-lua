88b4f8afad576b01b5941cbc5bce1bb35c056c03
laixintao <nanshi.lxt@alipay.com>
2018-12-02 23:20:50 +0800

    add test_function

---
+++ test_function.lua
@@ -0,0 +1,16 @@
+function foo(arg1)
+    print(arg1)
+end
+
+
+foo("hello")
+
+foo "world"
+
+
+function foo2(one, two)
+    print(one .. two .. "that\" it")
+end
+
+foo2("ni", "hao")
+foo2 "shoing"
